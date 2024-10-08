397aa2264bc8c937fbefd39113a9810b30fe5d4e
ArisKatsarakis <ariskatsarakis@hotmail.com>
2024-10-08 09:01:56 +0300

    added module colorcheme

--- nvim-config/init.lua
+++ nvim-config/init.lua
@@ -6,17 +6,17 @@ vim.cmd("set shiftwidth=2")
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -25,17 +25,14 @@ vim.opt.rtp:prepend(lazypath)
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
+local plugins = require("plugins.colorscheme")
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    {"shaunsingh/nord.nvim", opt = {}}
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "nord" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+	spec = {plugins},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "nord" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
-
-vim.cmd.colorscheme "nord"
+vim.cmd.colorscheme("nord")
--- nvim_lazyvim.Dockerfile
+++ nvim_lazyvim.Dockerfile
@@ -5,6 +5,6 @@ RUN pacman -S git neovim gcc gdb ripgrep lazygit fd unzip --noconfirm
 RUN pacman -S ttf-jetbrains-mono-nerd --noconfirm
 RUN mkdir ~/.config
 RUN mkdir ~/.config/nvim
-COPY ./nvim-config ~/.config/nvim/
+COPY ./nvim-config /root/.config/nvim/
 
 ENTRYPOINT ["bash"]
