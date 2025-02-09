ae5454364248cd005110ffedbe0954f6cc6a2791
ArisKatsarakis <ariskatsarakis@hotmail.com>
2025-02-09 15:41:15 +0200

    added formatting

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine 
 
-RUN apk update  && apk add gcc neovim  git g++ zlib zlib-dev  node npm 
+RUN apk update  && apk add gcc neovim  git g++ zlib zlib-dev  node npm  lua-language-server neovim-doc
 
 RUN mkdir -p /root/.config/
 
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,51 +1,7 @@
-
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
-vim.g.have_nerd_font = true
-
-vim.opt.number = true
-vim.opt.mouse = "a"
-
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
 vim.opt.clipboard = "unnamedplus"
-vim.opt.shiftwidth= 4
-
-vim.opt.breakindent = true
-
-
-vim.opt.undofile = true
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.signcolumn = "yes"
-
-vim.opt.updatetime = 250
-
-vim.opt.timeoutlen = 300
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
--- vim.opt.list = true
--- vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = "split"
-
-vim.opt.cursorline = true
-
-vim.opt.scrolloff = 20
+vim.opt.shiftwidth = 2
 
 require("config.lazy")
 
@@ -61,5 +17,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
-
-
