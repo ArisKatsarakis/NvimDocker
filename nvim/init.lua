0f35b1a3b63169d330a370f3ef443ed59720c077
ArisKatsarakis <ariskatsarakis@hotmail.com>
2025-02-09 17:41:36 +0200

    added a little terminal command

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,13 +7,33 @@ require("config.lazy")
 
 
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("n", "<space><space>x", ":source % <CR")
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
+
+
+local job_terminal_id = 0
+vim.keymap.set("n", "<leader>ft", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 25)
+
+  job_terminal_id = vim.bo.channel
+end)
+--TODO fix any errors
+-- build procect command
+vim.keymap.set("n", "<leader>bp", function()
+  -- make
+  -- mvn clean install , gradle install
+  vim.fn.chansend(job_terminal_id, { "ls \r\n" })
+end
+)
